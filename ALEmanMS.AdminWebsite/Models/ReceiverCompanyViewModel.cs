7f3d82244b942ee42916b50af22111b1f060f496
Semos5245 <s.alan.abd1998@gmail.com>
2019-02-14 17:47:17 +0200

    fixing display of senders and receivers companies fields

--- ALEmanMS.AdminWebsite/Models/ReceiverCompanyViewModel.cs
+++ ALEmanMS.AdminWebsite/Models/ReceiverCompanyViewModel.cs
@@ -10,17 +10,21 @@ namespace ALEmanMS.AdminWebsite.Models
     {
         [Required]
         [StringLength(50)]
+        [Display(Name = "اسم الشركة")]
         public string Name { get; set; }
 
         [Required]
         [StringLength(20)]
+        [Display(Name = "المدينة")]
         public string Country { get; set; }
 
         [Required]
         [StringLength(20)]
+        [Display(Name = "الرقم الوطني")]
         public string CommercialNumber { get; set; }
 
         [StringLength(512)]
+        [Display(Name = "الوصف")]
         public string Description { get; set; }
     }
 }
